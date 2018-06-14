c6d958550bde643938bf0159c2ea9cc3c5dabd72
wangjiangs <1123570027@qq.com>
2018-06-14 14:02:46 +0800

    响应式

---
+++ src/components/developers.vue
@@ -0,0 +1,292 @@
+<template>
+	<div>
+		<Header></Header>
+			<div class="developers">
+				<div class="developers-head">
+					<h3>DEVELOPERS</h3>
+				</div>
+				<div class="developers-doc">
+					<h3>Doc</h3>
+					<div class="heading">
+						<div class="line"></div>
+						<div class="content">Heading</div>
+						<div class="line"></div>
+						<div class="content">Heading</div>
+						<div class="line"></div>
+						<div class="content">Heading</div>
+						<div class="line"></div>
+					</div>
+				</div>
+				<div class="developers-tools">
+					<h3>Tools</h3>
+					<ul class="icon-list">
+						<li class="line"></li>
+						<li class="box box-1"><i class="icon iconfont">&#xe6d3;</i></li>
+						<li class="line"></li>
+						<li class="box box-2"><i class="icon iconfont">&#xe684;</i></li>
+						<li class="line"></li>
+						<li class="box box-3" @click="toFaucet"><i class="icon iconfont">&#xe60d;</i></li>
+						<li class="line"></li>
+						<li class="box box-4"><i class="icon iconfont">&#xe98c;</i></li>
+						<li class="line"></li>
+					</ul>
+				</div>
+			</div>
+		<Footer></Footer>
+	</div>
+</template>
+<script>
+	import Footer from './footer'
+	import Header from './header'
+
+	export default {
+		components:{
+			Header,
+			Footer
+		},
+		methods:{
+			toFaucet:function(){
+				this.$router.push('/faucet')
+			}
+		}
+	}
+</script>
+<style lang="less">
+	@media screen and (max-width:768px){html,body{font-size:20px !important}}
+	@media screen and (max-width:380px){html,body{font-size:10px !important}}
+	@font-face {
+	  font-family: 'iconfont';
+	  src: url('../assets/iconfont/developers/iconfont.eot');
+	  src: url('../assets/iconfont/developers/iconfont.eot?#iefix') format('embedded-opentype'),
+	  url('../assets/iconfont/developers/iconfont.woff') format('woff'),
+	  url('../assets/iconfont/developers/iconfont.ttf') format('truetype'),
+	  url('../assets/iconfont/developers/iconfont.svg#iconfont') format('svg');
+	}
+	.iconfont{
+	  font-family:"iconfont" !important;
+	  font-size:16px;font-style:normal;
+	  -webkit-font-smoothing: antialiased;
+	  -webkit-text-stroke-width: 0.2px;
+	  -moz-osx-font-smoothing: grayscale;
+	}
+	.developers{
+		// height:1070px;
+		font-family: AvenirLT-Heavy;
+		.developers-head{
+			height: 270px;
+			background-size: 100% 100%;
+			background-repeat: no-repeat;
+			background-position: center;
+			background-image: url(../assets/imgs/developers/banner.jpg);
+			@media (max-width: 768px) {
+				height: 10rem;
+			}
+			h3{
+				font-size: 36px;
+				text-align: center;
+				line-height: 270px;
+				color: #E4EEFF;
+				font-weight: normal;
+			}
+			@media (max-width: 768px) {
+				height: 10rem;
+				h3{
+					line-height: 10rem;
+					font-size: 1.8rem
+				}
+			}
+		}
+		.developers-doc{
+			overflow-y: hidden;
+			height: 357px;
+			background-size: 100% 100%;
+			background-image: url('../assets/imgs/developers/tbg.jpg');
+			@media (max-width: 768px){
+				height: 24.9rem;
+				position: relative;
+			}
+			h3{
+
+				color:#202020;
+				font-size: 32px;
+				margin-top: 106px;
+				text-align: center;
+				@media (max-width: 768px){
+					margin-top: 3.85rem;
+					font-size: 1.8rem;
+				}
+			}
+			.heading{
+				width: 1154px;
+				height: 30px;
+				margin: 10px auto 0 auto;
+				overflow-y: hidden;
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				@media (max-width: 768px){
+					width: calc(100% - 3rem);
+					display: block;
+					height: 19.7rem;
+				}
+				div.line{
+					height: 17px;
+					width: 1px;
+					background-color: #e8e8e8;
+					@media (max-width: 768px){
+						display: none
+					}
+				}
+				div.content{
+					width: 40px;
+					height: 22px;
+					font-size: 16px;
+					font-weight: bold;
+					color: #333;
+					transition: all .25s ease-in-out;
+
+					&:hover{
+						transform: scale(1.25);
+						color: #1346A2;
+					}
+					@media (max-width: 768px){
+						text-align: center;
+						width: 100%;
+						height: 6.05rem;
+						line-height: 6.05rem;
+						font-size: 1.7rem;
+						font-weight: normal;
+						&:nth-child(4){
+							border-width: 1px 0;
+							border-style: solid;
+							border-color: rgba(51,51,51,.1);
+						}
+					}
+				}
+			}
+		}
+		.developers-tools{
+			height: 442px;
+			overflow-y: hidden;
+			@media (max-width: 768px){
+				height: 35.7rem;
+			}
+
+			h3{
+
+				color:#202020;
+				font-size: 32px;
+				margin-top: 83px;
+				text-align: center;
+				@media (max-width: 768px){
+					margin-top: 5.15rem;
+					font-size: 1.8rem;
+				}
+			}
+			ul.icon-list{
+				list-style-type:none;
+				margin: 84px auto 0 auto;
+				padding: 0; 
+				width: 1160px;
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				height: 111px;
+				@media (max-width: 768px) {
+					width: 100%;
+					justify-content: center;
+					align-content: flex-start;
+					flex-wrap: wrap;
+					margin-top: 3.1rem
+				}
+				li.line{
+					width: 1px;
+					height: 46px;
+					background-color: #e8e8e8;
+					@media (max-width: 768px) {
+						display: none
+					}
+				}
+				li.box{
+					width: 70px;
+					height: 70px;
+					display: flex;
+					position: relative;
+					justify-content: center;
+					align-items: center;
+					@media (max-width: 768px) {
+						
+						width: 17.25rem;
+						height: 10rem;
+					    border: solid 1px #e3e3e3;
+					    border-collapse: collapse;
+					    align-items: flex-start;
+					}
+					i{
+						display: inline-block;
+						width: 50px;
+						height: 50px;
+						line-height: 50px;
+						font-size: 50px;
+						color: #989898;
+						transition: all .25s ease-in-out;
+						cursor: pointer;
+						user-select: none;
+						&:hover{
+							transform: scale(1.36);
+							color: #1346A2
+						}
+						@media (max-width: 768px) {
+							width: 3rem;
+							height: 3rem;
+							font-size: 3rem;
+							line-height: 3rem;
+							margin-top: 2.4rem;
+						}
+
+					}
+				}
+				li.box::after{
+					position: absolute;
+					color:#202020;
+					font-size: 1.5rem;
+					font-weight: bold;
+					bottom: -40px;
+					@media (max-width: 768px){
+						bottom: 2rem;
+						font-weight: normal;
+					}
+				}
+				li.box-1{
+					margin: 0 -1px -1px 0
+				}
+				li.box-4{
+					margin: -1px 0 0 -1px
+				}
+				li.box-3{
+					margin-top: -1px
+				}
+				li.box-1::after{
+					content:"Monitor";
+				}
+				li.box-2::after{
+					content:"Explorer";
+				}
+				li.box-3::after{
+					content:"Faucet";
+				}
+				li.box-4::after{
+					content:"Wallet";
+				}
+
+			}
+		}
+	}
+	@media screen and (max-width: 768px){
+		.developers{
+			.developers-head{
+				background-image: url(../assets/imgs/developers/mbanner.jpg);
+			}
+		}
+	}
+</style>
---
+++ src/components/faucet.vue
@@ -0,0 +1,115 @@
+<template>
+	<div>
+		<Header></Header>
+			<div class="faucet">
+				<h3>Faucet</h3>
+				<p class="title">Address in TestNet</p>
+				<input type="text" name="">
+				<p class="tips">一个地址只能领取一次</p>
+				<button>Get 10 Seeles</button>
+			</div>
+		<Footer></Footer>
+	</div>
+</template>
+<script>
+	import Footer from './footer'
+	import Header from './header'
+
+	export default {
+		components:{
+			Header,
+			Footer
+		},
+		methods:{
+		}
+	}
+</script>
+<style lang="less">
+	.faucet{
+		height: 800px;
+		font-family:AvenirLT-Heavy;
+		@media (max-width: 768px){
+				height: 39rem;
+		}
+		h3{
+			font-size: 32px;
+			color: #202020;
+			text-align: center;
+			margin-top: 117px;
+			@media (max-width: 768px)  {
+				  margin-top: 4.45rem;
+				  font-size: 1.8rem;
+			}  
+		}
+		p.title{
+			font-size: 18px;
+			color: #333;
+			font-family:AvenirLT-Roman;
+			text-align: center;
+			margin-top: 120px;
+			@media (max-width: 768px){
+				font-size: 1.5rem;
+				margin-top: 5.55rem;
+			}
+		}
+		input{
+			width: 580px;
+			height: 70px;
+			display: block;
+			margin: 30px auto 0 auto;
+			padding:1.2rem;
+			font-size: 1.5rem;
+			box-sizing: border-box;
+			border-radius: 5px;
+			outline: none;
+			border: solid 1px #d3d3d3;
+			transition: all .5s ease-in-out;
+			@media (max-width: 768px){
+				// width: 36rem;
+				// height: 5rem;
+				width: 29rem;
+				height: 4rem;
+				margin-top: 2rem;
+			}
+		}
+		input:focus{
+			border-color: #1346A2;
+		}
+		p.tips{
+			color: #999;
+			font-size: 14px;
+			margin-top: 26px;
+			text-align: center;
+			@media (max-width: 768px){
+				font-size: 1.4rem;
+				margin-top: 1.4rem;
+			}
+		}
+		button{
+			width: 160px;
+			height: 50px;
+			margin: 62px auto 0 auto;
+			display: block;
+			background-color: #1346A2;
+			border-radius: 5px;
+			border:0;
+
+			font-size: 16px;
+			font-weight: bold;
+			color: white;
+			font-family:AvenirLT-Heavy;
+			cursor: pointer;
+			&:active{
+				opacity: .85
+			};
+			@media (max-width: 768px){
+			    width: 12.5rem;
+			    height: 4rem;
+			    font-size: 1.5rem;
+			    margin-top: 3.6rem;
+			}
+		}
+
+	}
+
+</style>
